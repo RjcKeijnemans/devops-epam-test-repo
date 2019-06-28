048798f71dd21dac63cfebbf998bde546369af4c
John Doe <johndoe@example.com>
2019-06-28 14:12:04 +0200

    Add db setup ps script

--- db_setup.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem "db.sql"| `
-Foreach-Object{
-    Write-Host 'Executing' $_.FullName
-    sqlcmd -U api_db_user@epamtest -P api_db_password -S epamtest.database.windows.net -d api_db -i $_.FullName
-}
