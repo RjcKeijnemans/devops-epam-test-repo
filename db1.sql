842ec67dcaa9baae8fb15953a6a8682e72bb2b37
John Doe <johndoe@example.com>
2019-07-01 14:43:14 +0200

    heeeeelp

--- db1.sql
+++ db1.sql
@@ -1,6 +1,5 @@
-CREATE USER api_db_user FOR LOGIN api_db_user
+USE api_db
 GO
 
-ALTER ROLE db_owner ADD MEMBER api_db_user;
---ALTER AUTHORIZATION ON DATABASE ::api_db TO api_db_user;
+CREATE USER api_db_user FOR LOGIN api_db_user
 GO
