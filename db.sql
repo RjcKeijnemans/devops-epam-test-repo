81980ce8cccd33a69422c94d6fcfb45017cff34d
John Doe <johndoe@example.com>
2019-07-01 12:05:25 +0200

    Try again

--- db.sql
+++ db.sql
@@ -2,6 +2,7 @@ CREATE LOGIN api_db_user WITH PASSWORD = 'api_db_password';
 GO
 
 ALTER AUTHORIZATION ON DATABASE ::api_db TO api_db_user;
+GRANT ALL PRIVILEGES ON api_db to api_db_user;
 GO
 
 USE api_db
