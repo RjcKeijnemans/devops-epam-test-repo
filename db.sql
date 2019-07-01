6c673845563e036aae08304a6d134d11f0db67e0
John Doe <johndoe@example.com>
2019-07-01 14:10:09 +0200

    help

--- db.sql
+++ db.sql
@@ -7,8 +7,6 @@ GO
 CREATE USER [api_db_user] FOR LOGIN [api_db_user] WITH DEFAULT_SCHEMA=[dbo]
 GO
 
-ALTER ROLE db_owner ADD MEMBER api_db_user;
-
 CREATE DATABASE api_db
 ALTER AUTHORIZATION ON DATABASE ::api_db TO api_db_user
 GO
