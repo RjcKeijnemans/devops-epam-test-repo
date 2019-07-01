a5e2db266d4606559344a9bac1b3149c52001ae4
John Doe <johndoe@example.com>
2019-07-01 14:01:15 +0200

    pls

--- db_initialization.ps1
+++ db_initialization.ps1
@@ -32,9 +32,16 @@ $serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGr
     -ServerName $serverName `
     -FirewallRuleName "AllowedIPs" -StartIpAddress $startIp -EndIpAddress $endIp
     
-# Execute database script
+# Create database and user credentials
 Get-ChildItem "db.sql"| `
 Foreach-Object{
     Write-Host 'Executing' $_.FullName
     sqlcmd -U $adminSqlLogin@$serverName -P $password -S $DBURL -d "master" -i $_.FullName
+}
+
+# Login into database and execute script
+Get-ChildItem "db1.sql"| `
+Foreach-Object{
+    Write-Host 'Executing' $_.FullName
+    sqlcmd -U "api_db_user" -P "Interforaewg098!" -S $DBURL -d "api_db" -i $_.FullName
 }
